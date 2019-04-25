ba546387a8981f190d40becb9eaac652db7f6f58
ben <bm@wps.de>
2019-04-25 21:58:38 +0200

    cleans up

--- one.R
+++ one.R
@@ -1,6 +1,6 @@
 # Project 1
 
-# Question 1
+################################## Question 1
 # 0 = tails, 1 = heads
 firstUrn <- c(0,1)
 n <- 1
@@ -15,7 +15,6 @@ result_one_toss <- sample(0:1,1)
 
 # Hier brauchen wir ein replace im sample ansonsten funktioniert es nicht
 result_n_tosses <- sample(0:1,nsamples,replace=TRUE)
-# result_n_discrete_Tosses <- replicate(nsamples, sample(firstUrn,n,TRUE))
 
 resultOneBinom = rbinom(nsamples,1,0.5) 
 
@@ -26,21 +25,22 @@ hist(result_one_toss, main="One Toss")
 hist(resultOneBinom, xlab="Binomial Toss")
 
 
-# Question 2 a.)
+################################## Question 2 a.)
 
 # OMEGA=1:6, druckt eine Liste 1,2,3,4,5,6 ist ganz praktisch
 OMEGA_2a <- 1:6
 num_of_dice_2a <- 4
+N_2a <- 10^5
 
 # simuliere hier lediglich die 4 simultanen Wuerfe
 # Wenn ihr den Befehl ohne hist ausfuehrt seht ihr die Tabelle, passt genau auf die Anfrage
-toss_dice_2a <- replicate(num_of_dice_2a,sample(OMEGA_2a,1))
+toss_dice_2a <- replicate(num_of_dice_2a,sample(OMEGA_2a,N_2a,replace=TRUE))
 
 # Nur plotting
 hist(toss_dice_2a, main="Question 2a", xlab="Dice value", ylab="# of throws")
 
 
-# Question 2 b.)
+################################### Question 2 b.)
 OMEGA_2b <- 1:6
 num_of_dice_2b <- 2
 num_of_events_2b <- 24
