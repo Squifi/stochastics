1da2540c7634a72a6ef753da050654370b8a57b8
ben <bm@wps.de>
2019-05-21 13:36:41 +0200

    finishes Question 3/Sheet2

--- two.R
+++ two.R
@@ -21,7 +21,28 @@ exercise_2 <- qexp(U,lambda)
 hist(exercise_2, freq=FALSE)
 
 x <- seq(0,max(exercise_2),.1)
-lines(x,dexp(x))
+lines(x,dexp(x,2))
 
 
 # Aufgabe 3:
+
+n = 10^5
+radius = sqrt(rexp(n,.5))
+angle = runif(n,0,2*pi)
+X1 <- radius * cos(angle)
+
+hist(X1,freq=FALSE)
+
+x <- seq(min(X1), max(X1),.1)
+lines(x,dnorm(x))
+
+# Aufgabe 4
+SEED <- as.integer(Sys.time())
+lcg.recursive <- function(a,b,m,n,x_k) {
+	return ((a*x_k+b)%%m)
+}
+
+LCG <- function(a,b,m,n,SEED) {
+	x_0 <- SEED	
+}
+		
